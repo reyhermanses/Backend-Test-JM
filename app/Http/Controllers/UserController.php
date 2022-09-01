a6426a9604ce73f93d0d6dfb9f8767594319efea
reyhermanses <rey.undot@gmail.com>
2022-08-31 21:19:55 -0700

    exception auth for login dan register user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:api');
+        $this->middleware('auth:api')->except(['register','login']);
         $this->middleware('auth.admin')->only(['index']);
     }
 
